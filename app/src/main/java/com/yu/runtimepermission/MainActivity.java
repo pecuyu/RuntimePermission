0dc00dc4e4b758867a29acbcac58aaeae3216a75
unknown <D22436@SZCKT.COM>
2017-08-02 14:31:49 +0800

    add runtime permission check

--- app/src/main/java/com/yu/runtimepermission/MainActivity.java
+++ app/src/main/java/com/yu/runtimepermission/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,25 +20,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void callPhone(View view) {
-        Intent intent = new Intent(Intent.ACTION_CALL);
-        intent.setData(Uri.parse("tel:10086"));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            // 没有允许则申请权限，最终结果在onRequestPermissionsResult处理
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 6);
             return;
         }
-        startActivity(intent);
-
+        call();  //权限允许则直接打电话
     }
+
+    public void call() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:10086"));
+        startActivity(intent);
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 6:
+                // 申请权限结果保存在grantResults数组中，进行判断
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call();
+                } else {
+                    Toast.makeText(this, "you denied the permission!",Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
 
     }
 }
